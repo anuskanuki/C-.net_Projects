10c5293c8c08bd9dfca3d4af141e903887ba2ec9
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-09-11 17:16:37 -0300

    Commit - Class september 11

---
+++ ArrayClass/ArrayClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arrayLinear = new string[3] { "Nuki", "Guilherme", "Josecleison" };
+
+            arrayLinear[0] = "Nuki";
+            arrayLinear[1] = "Guilherme";
+            arrayLinear[2] = "Josecleison";
+
+            List<string> linearList = new List<string>();
+
+            //CTRL + point   show the suggestions when you have underlined a "...". press ENTER to make those automaticly on the code
+            linearList.Add("Nuki");
+            linearList.Add("Guilherme");
+            linearList.Add("Josecleison");
+
+
+            Console.WriteLine(arrayLinear[0]);
+            Console.WriteLine(arrayLinear[1]);
+            Console.WriteLine(arrayLinear[2]);
+
+           // var keyPress = Console.ReadKey().KeyChar.ToString();
+
+
+        }
+    }
+}
---
+++ StringTypesConcat/StringTypesConcat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTypesConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type:\nNAME AND PRESS ENTER\nAge AND PRESS ENTER\nDESCRIPTION AND PRESS ENTER\n    ");
+
+            var templateTest = @"User name: {0} 
+Age: {1}
+Description: {2}";
+
+
+            var finishText = string.Format(templateTest
+                , Console.ReadLine()
+                , Console.ReadLine()
+                , Console.ReadLine());
+
+                                   
+            Console.WriteLine(finishText);
+            
+            Console.ReadKey();
+
+
+                        
+
+        }
+    }
+}
