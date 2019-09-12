a34ae4728c4edafe8b107a0ca903cd48af34055b
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-09-12 17:01:45 -0300

    CommitClass12/sep

---
+++ RepeatLoops/UsingWhileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            var par = 4;
+            while (count<=100)
+            {
+                if (count % 2 == 0)
+                {
+                Console.WriteLine(count.ToString().Replace(count.ToString(),par.ToString()));
+                }
+                Console.WriteLine(count++);
+            }
+            Console.WriteLine("Game over");
+            Console.ReadKey();
+
+        }
+    }
+}
