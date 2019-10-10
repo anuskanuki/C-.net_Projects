6094f4e7673fa73eb43b1bab7ad3ef14a8294c02
Anuska Kepler Rehn <anuska.rehn@gmail.com>
2019-10-10 10:53:08 -0300

    Folders Organization

---
+++ Folder- Scripts_DataBase/SQLQuery7.sql
@@ -0,0 +1,7 @@
+﻿select * from Pedidos
+select * from Clientes
+
+SELECT * from Pedidos p
+where p.ClienteId in(select c.Id from Clientes c
+inner join Pedidos p on c.Id = p.ClienteId
+where p.Numero in('N0000008','N0000006' ,'N0000003'))order by p.Valor desc     
